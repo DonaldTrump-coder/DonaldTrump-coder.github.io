29e5483c06ef4f0212d9a685698f76f18fe32a6b
DonaldTrump-coder <3279179450@qq.com>
2026-04-19 17:14:14 +0800

    update music

--- actions.js
+++ actions.js
@@ -2,12 +2,14 @@ window.addEventListener('DOMContentLoaded', function () {
     const audio = document.getElementById('music');
     if (!audio) return;
 
+    audio.muted = true;
     audio.play().catch(() => {
         console.log("Failed to auto-play");
     });
 
     const unlockAudio = () => {
         audio.muted = false;
+        audio.play().catch(() => {});
         console.log("Music unmuted!");
         document.removeEventListener('click', unlockAudio);
         document.removeEventListener('keydown', unlockAudio);
