c2e9576769fd16a64d75730801fe005eeb75646a
DonaldTrump-coder <3279179450@qq.com>
2026-04-19 16:26:42 +0800

    add music

--- actions.js
+++ actions.js
@@ -6,13 +6,19 @@ window.addEventListener('DOMContentLoaded', function () {
     audio.play().catch(() => {
         const startMusic = () => {
             audio.muted = false;
-            audio.play();
+            audio.play().then(() => {
+                console.log("Music started!");
+            }).catch((err) => {
+                console.log("Error playing music:", err);
+            });
 
             document.removeEventListener('click', startMusic);
             document.removeEventListener('keydown', startMusic);
+            document.removeEventListener('scroll', startMusic);
         };
 
         document.addEventListener('click', startMusic);
         document.addEventListener('keydown', startMusic);
+        document.addEventListener('scroll', startMusic);
     });
 });
